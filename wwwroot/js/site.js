d49eff0eac78183197deae32e7e0fe83da3cc836
eraldo zela <zelaeraldo7@gmail.com>
2026-02-21 00:48:31 +0100

    Add files via upload

---
+++ wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿document.getElementById('faqLink').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.location.href = "https://localhost:7060/FAQs";
+});
+
+document.getElementById('categoriesLink').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.location.href = "https://localhost:7060/EventCategories";
+});
+document.querySelectorAll('.ticket-btn').forEach(button => {
+    button.addEventListener('click', function (e) {
+        e.preventDefault();
+        window.open('https://localhost:7060/Tickets', '_blank');
+    });
+});
